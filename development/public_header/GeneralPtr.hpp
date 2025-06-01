0ccf691fff3abf9cd648a61e049fd715e9b6dbbc
furkanozen <furkanoozen4@gmail.com>
2025-06-01 21:36:07 +0300

    source codes are added

--- development/public_header/GeneralPtr.hpp
+++ development/public_header/GeneralPtr.hpp
@@ -1,9 +1,52 @@
 #include <iostream>
 
+template<typename PointerType>
+class GenericPointer {
+private:
+
+    PointerType * m_GenericPointer;
 
 
-class Furkan {
-    public:
-        Furkan()= default;
-        ~Furkan() = default;
+
+public:
+
+    GenericPointer():m_GenericPointer(nullptr) {
+
+    }
+
+    GenericPointer(PointerType* p): m_GenericPointer(p) {}
+
+    GenericPointer (const GenericPointer& otherGenericPointer) = delete;
+
+    GenericPointer (GenericPointer&& otherGenericPointer) noexcept {
+        m_GenericPointer = otherGenericPointer.m_GenericPointer;
+        otherGenericPointer. m_GenericPointer= nullptr;
+    }
+
+    GenericPointer& operator=( const GenericPointer& otherGenericPointer) = delete;
+
+    GenericPointer& operator=(GenericPointer&& otherGenericPointer) noexcept {
+        if(this != &otherGenericPointer) {
+            delete m_GenericPointer;
+            m_GenericPointer = otherGenericPointer.m_GenericPointer;
+            otherGenericPointer.m_GenericPointer = nullptr;
+        }
+
+        return *this;
+    }
+
+
+    ~GenericPointer() {
+        delete m_GenericPointer;
+    }
+
+    PointerType* operator->() const {
+        return m_GenericPointer;
+    }
+
+    PointerType& operator*() const {
+        return *m_GenericPointer;
+    }
+
 };
+
--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,21 @@
 #include "GeneralPtr.hpp"
 
 int main() {
-    std::cout<<"Hello"<<std::endl;
 
-    Furkan furkan;
+    int* x = new int(78);
+
+    int* a = new int(78);
+
+    GenericPointer hop{x};
+    GenericPointer selam{a};
+    selam = std::move(hop);
+
+    if( x == nullptr ){
+        std::cout <<" lAn oglummmm"<<std::endl;
+    }
+
+    std::cout<<"Sekam"<<std::endl;
+
+    
+
 }
